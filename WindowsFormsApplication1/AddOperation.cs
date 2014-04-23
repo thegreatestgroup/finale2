bea2166f14d88694ce867188fa0ea3d1baf67577
Daniel Puckowski <puckowski.d@gmail.com>
2014-04-23 19:49:27 -0400

    Added simple insert class.

---
+++ WindowsFormsApplication1/AddOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class AddOperation
+    {
+        public void addAttributeToTable(string tableName, string listOfValues)
+        {
+            string insertQuery = "INSERT INTO " + tableName;
+            insertQuery += " VALUES (" + listOfValues + ");";
+
+            SQLConnection sqlConnection = new SQLConnection();
+
+            sqlConnection.queryDatabase(tableName, insertQuery);
+        }
+    }
+}
--- WindowsFormsApplication1/SQLConnection.cs
+++ WindowsFormsApplication1/SQLConnection.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsApplication1
+namespace WindowsFormsApplication2
 {
     public class SQLConnection
     {
