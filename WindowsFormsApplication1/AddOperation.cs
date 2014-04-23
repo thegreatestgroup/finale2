bc4fe088d8d885731c9def41ad92df0f0fd30f80
Daniel Puckowski <puckowski.d@gmail.com>
2014-04-23 19:57:40 -0400

    Updated insert with transactions.

--- WindowsFormsApplication1/AddOperation.cs
+++ WindowsFormsApplication1/AddOperation.cs
@@ -8,10 +8,27 @@ namespace WindowsFormsApplication2
 {
     public class AddOperation
     {
-        public void addAttributeToTable(string tableName, string listOfValues)
+        public void addAttributeToTableWithoutTransaction(string tableName, string listOfValues)
         {
             string insertQuery = "INSERT INTO " + tableName;
             insertQuery += " VALUES (" + listOfValues + ");";
+
+            SQLConnection sqlConnection = new SQLConnection();
+
+            sqlConnection.queryDatabase(tableName, insertQuery);
+        }
+
+        public void addAttributeToTable(string tableName, string listOfValues)
+        {
+            string insertQuery = "DECLARE @TranName VARCHAR(20);" +
+                " SELECT @TranName = 'InsertTransaction';" +
+                " BEGIN TRANSACTION @TranName;";
+            
+            insertQuery += " INSERT INTO " + tableName;
+            insertQuery += " VALUES (" + listOfValues + ");";
+
+            insertQuery += " COMMIT TRANSACTION @TranName;" +
+                " GO";
 
             SQLConnection sqlConnection = new SQLConnection();
 
