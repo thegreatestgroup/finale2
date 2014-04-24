7c7e59c9594605747eed911b1492d277a07e05b1
Daniel Puckowski <puckowski.d@gmail.com>
2014-04-23 20:00:56 -0400

    Added simple delete w/o transactions.

---
+++ WindowsFormsApplication1/DeleteOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class DeleteOperation
+    {
+        public void deleteAttributeFromTableWithoutTransaction(string tableName, string columnName, string valueToDelete)
+        {
+            string deleteQuery = "DELETE FROM " + tableName;
+            deleteQuery += " WHERE " + columnName + "=" + valueToDelete + ";";
+
+            SQLConnection sqlConnection = new SQLConnection();
+
+            sqlConnection.queryDatabase(tableName, deleteQuery);
+        }
+    }
+}
